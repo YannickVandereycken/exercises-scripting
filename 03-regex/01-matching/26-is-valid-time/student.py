cd7cc00ad648f4b8ba6e7ee11a4b9fe463eb9d5c
YannickVandereycken <91282328+YannickVandereycken@users.noreply.github.com>
2022-03-02 15:29:21 +0100

    oef 2-3-22

--- 03-regex/01-matching/26-is-valid-time/student.py
+++ 03-regex/01-matching/26-is-valid-time/student.py
@@ -1,1 +1,4 @@
 # Write your code here
+import re
+def is_valid_time(string):
+    return re.fullmatch('[0-9][0-9]:[0-9][0-9]:[0-9][0-9](\\.[0-9][0-9][0-9])?',string)
--- 03-regex/01-matching/28-is-valid-password/student.py
+++ 03-regex/01-matching/28-is-valid-password/student.py
@@ -1,1 +1,4 @@
 # Write your code here
+import re
+def is_valid_password(string):
+    return re.search('.{8,}|',string)
